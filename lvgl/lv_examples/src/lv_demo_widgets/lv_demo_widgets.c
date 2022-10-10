f2dd5fc24d5fb91de9b3491d6beac75f810a0078
AlesCicero <a.cicero@studenti.unibg.it>
2022-10-10 12:07:15 +0200

    modify

--- lvgl/lv_examples/src/lv_demo_widgets/lv_demo_widgets.c
+++ lvgl/lv_examples/src/lv_demo_widgets/lv_demo_widgets.c
@@ -202,7 +202,7 @@ static void controls_create(lv_obj_t * parent)
 
 static void visuals_create(lv_obj_t * parent)
 {
-    lv_page_set_scrl_layout(parent, LV_LAYOUT_PRETTY_TOP);
+    //lv_page_set_scrl_layout(parent, LV_LAYOUT_PRETTY_TOP);
 
     lv_disp_size_t disp_size = lv_disp_get_size_category(NULL);
 
