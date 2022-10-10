190e8bf095bf288589c747dd83fd555b0aee633f
AlesCicero <a.cicero@studenti.unibg.it>
2022-10-10 15:26:41 +0200

    Modify

--- lvgl/lv_examples/src/lv_demo_widgets/lv_demo_widgets.c
+++ lvgl/lv_examples/src/lv_demo_widgets/lv_demo_widgets.c
@@ -63,7 +63,7 @@ static lv_style_t style_box;
 static void event_handler(lv_obj_t* obj, lv_event_t event)
 {
     if (event == LV_EVENT_VALUE_CHANGED) {
-        
+        lv_btn_set_fit2(btn2, LV_FIT_NONE, LV_FIT_TIGHT);
     }
 }
 
@@ -77,7 +77,7 @@ void lv_demo_widgets(void)
     lv_obj_align(btn2, NULL, LV_ALIGN_CENTER, 0, 100);
     lv_btn_set_checkable(btn2, true);
     lv_btn_toggle(btn2);
-    lv_btn_set_fit2(btn2, LV_FIT_NONE, LV_FIT_TIGHT);
+    
 
     label = lv_label_create(btn2, NULL);
     lv_label_set_text(label, "Toggled");
