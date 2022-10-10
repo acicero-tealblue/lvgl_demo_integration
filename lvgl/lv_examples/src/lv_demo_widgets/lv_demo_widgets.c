03d37180293f14a16576c959781ddd869e6cd286
AlesCicero <a.cicero@studenti.unibg.it>
2022-10-10 15:32:55 +0200

    m

--- lvgl/lv_examples/src/lv_demo_widgets/lv_demo_widgets.c
+++ lvgl/lv_examples/src/lv_demo_widgets/lv_demo_widgets.c
@@ -15,31 +15,7 @@
  *      DEFINES
  *********************/
 
-/**********************
- *      TYPEDEFS
- **********************/
 
-/**********************
- *  STATIC PROTOTYPES
- **********************/
-static void controls_create(lv_obj_t * parent);
-static void visuals_create(lv_obj_t * parent);
-static void selectors_create(lv_obj_t * parent);
-static void slider_event_cb(lv_obj_t * slider, lv_event_t e);
-static void ta_event_cb(lv_obj_t * ta, lv_event_t e);
-static void kb_event_cb(lv_obj_t * ta, lv_event_t e);
-static void bar_anim(lv_task_t * t);
-static void arc_anim(lv_obj_t * arc, lv_anim_value_t value);
-static void linemeter_anim(lv_obj_t * linemeter, lv_anim_value_t value);
-static void gauge_anim(lv_obj_t * gauge, lv_anim_value_t value);;
-static void table_event_cb(lv_obj_t * table, lv_event_t e);
-#if LV_USE_THEME_MATERIAL
-static void color_chg_event_cb(lv_obj_t * sw, lv_event_t e);
-#endif
-#if LV_DEMO_WIDGETS_SLIDESHOW
-static void tab_content_anim_create(lv_obj_t * parent);
-static void tab_changer_task_cb(lv_task_t * task);
-#endif
 
 /**********************
  *  STATIC VARIABLES
@@ -52,18 +28,15 @@ static lv_obj_t * kb;
 
 static lv_style_t style_box;
 
-/**********************
- *      MACROS
- **********************/
 
-/**********************
- *   GLOBAL FUNCTIONS
- **********************/
 
 static void event_handler(lv_obj_t* obj, lv_event_t event)
 {
-    if (event == LV_EVENT_VALUE_CHANGED) {
-        lv_btn_set_fit2(obj, LV_FIT_NONE, LV_FIT_TIGHT);
+    if (event == LV_EVENT_CLICKED) {
+        printf("Clicked\n");
+    }
+    else if (event == LV_EVENT_VALUE_CHANGED) {
+        printf("Toggled\n");
     }
 }
 
@@ -71,112 +44,24 @@ static void event_handler(lv_obj_t* obj, lv_event_t event)
 void lv_demo_widgets(void)
 {
     lv_obj_t* label;
-    
+
+    lv_obj_t* btn1 = lv_btn_create(lv_scr_act(), NULL);
+    lv_obj_set_event_cb(btn1, event_handler);
+    lv_obj_align(btn1, NULL, LV_ALIGN_CENTER, 0, -40);
+
+    label = lv_label_create(btn1, NULL);
+    lv_label_set_text(label, "Button");
+
     lv_obj_t* btn2 = lv_btn_create(lv_scr_act(), NULL);
     lv_obj_set_event_cb(btn2, event_handler);
-    lv_obj_align(btn2, NULL, LV_ALIGN_CENTER, 0, 100);
+    lv_obj_align(btn2, NULL, LV_ALIGN_CENTER, 0, 40);
     lv_btn_set_checkable(btn2, true);
     lv_btn_toggle(btn2);
-    
+    lv_btn_set_fit2(btn2, LV_FIT_NONE, LV_FIT_TIGHT);
 
     label = lv_label_create(btn2, NULL);
     lv_label_set_text(label, "Toggled");
 
-    //controls_create(t1);
-
-//    tv = lv_tabview_create(lv_scr_act(), NULL);
-//#if LV_USE_THEME_MATERIAL
-//    if(LV_THEME_DEFAULT_INIT == lv_theme_material_init) {
-//        lv_disp_size_t disp_size = lv_disp_get_size_category(NULL);
-//        if(disp_size >= LV_DISP_SIZE_MEDIUM) {
-//            lv_obj_set_style_local_pad_left(tv, LV_TABVIEW_PART_TAB_BG, LV_STATE_DEFAULT, LV_HOR_RES / 2);
-//            lv_obj_t * sw = lv_switch_create(lv_scr_act(), NULL);
-//            lv_obj_set_event_cb(sw, color_chg_event_cb);
-//            lv_obj_set_pos(sw, LV_DPX(10), LV_DPX(10));
-//            lv_obj_set_style_local_value_str(sw, LV_SWITCH_PART_BG, LV_STATE_DEFAULT, "Dark");
-//            lv_obj_set_style_local_value_align(sw, LV_SWITCH_PART_BG, LV_STATE_DEFAULT, LV_ALIGN_OUT_RIGHT_MID);
-//            lv_obj_set_style_local_value_ofs_x(sw, LV_SWITCH_PART_BG, LV_STATE_DEFAULT, LV_DPI/35);
-//        }
-//    }
-//#endif
-
-    //t1 = lv_tabview_add_tab(tv, "ALESSIO");
-    //t2 = lv_tabview_add_tab(tv, "Visuals");
-    //t3 = lv_tabview_add_tab(tv, "Selectors");
-
-
-    //lv_style_init(&style_box);
-    //lv_style_set_value_align(&style_box, LV_STATE_DEFAULT, LV_ALIGN_OUT_TOP_LEFT);
-    //lv_style_set_value_ofs_y(&style_box, LV_STATE_DEFAULT, - LV_DPX(10));
-    //lv_style_set_margin_top(&style_box, LV_STATE_DEFAULT, LV_DPX(30));
-
-    
-    //visuals_create(t2);
-    //selectors_create(t3);
-
-
 }
-
-/**********************
- *   STATIC FUNCTIONS
- **********************/
-
-
-static void controls_create(lv_obj_t * parent)
-{
-    //lv_page_set_scrl_layout(parent, LV_LAYOUT_PRETTY_TOP);
-
-    lv_disp_size_t disp_size = lv_disp_get_size_category(NULL);
-    lv_coord_t grid_w = lv_page_get_width_grid(parent, disp_size <= LV_DISP_SIZE_SMALL ? 1 : 2, 1);
-
-#if LV_DEMO_WIDGETS_SLIDESHOW == 0
-    static const char * btns[] = {"Cancel", "Ok", ""};
-
-    lv_obj_t * m = lv_msgbox_create(lv_scr_act(), NULL);
-    lv_msgbox_add_btns(m, btns);
-    lv_obj_t * btnm = lv_msgbox_get_btnmatrix(m);
-    lv_btnmatrix_set_btn_ctrl(btnm, 1, LV_BTNMATRIX_CTRL_CHECK_STATE);
-#endif
-
-    lv_obj_t * h = lv_cont_create(parent, NULL);
-    lv_cont_set_layout(h, LV_LAYOUT_PRETTY_MID);
-    lv_obj_add_style(h, LV_CONT_PART_MAIN, &style_box);
-    lv_obj_set_drag_parent(h, true);
-
-    lv_obj_set_style_local_value_str(h, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, "Basics");
-
-    lv_cont_set_fit2(h, LV_FIT_NONE, LV_FIT_TIGHT);
-    lv_obj_set_width(h, grid_w);
-    lv_obj_t * btn = lv_btn_create(h, NULL);
-    lv_btn_set_fit2(btn, LV_FIT_NONE, LV_FIT_TIGHT);
-    lv_obj_set_width(btn, lv_obj_get_width_grid(h, disp_size <= LV_DISP_SIZE_SMALL ? 1 : 2, 1));
-    lv_obj_t * label = lv_label_create(btn, NULL);
-    lv_label_set_text(label ,"Button");
-    
-    //lv_obj_set_event_cb(btn, kb_event_cb);
-    
-    //lv_checkbox_create(h, NULL);
-
-    lv_coord_t fit_w = lv_obj_get_width_fit(h);
-
-    lv_obj_t * slider = lv_slider_create(h, NULL);
-    lv_slider_set_value(slider, 40, LV_ANIM_OFF);
-    lv_obj_set_event_cb(slider, slider_event_cb);
-    lv_obj_set_width_margin(slider, fit_w);
-
-    
-    slider = lv_slider_create(h, slider);
-    lv_slider_set_type(slider, LV_SLIDER_TYPE_RANGE);
-    lv_slider_set_value(slider, 70, LV_ANIM_OFF);
-    lv_slider_set_left_value(slider, 30, LV_ANIM_OFF);
-    lv_obj_set_style_local_value_ofs_y(slider, LV_SLIDER_PART_INDIC, LV_STATE_DEFAULT, - LV_DPX(25));
-    lv_obj_set_style_local_value_font(slider, LV_SLIDER_PART_INDIC, LV_STATE_DEFAULT, lv_theme_get_font_small());
-    lv_obj_set_style_local_value_opa(slider, LV_SLIDER_PART_INDIC, LV_STATE_DEFAULT, LV_OPA_COVER);
-    lv_obj_set_event_cb(slider, slider_event_cb);
-    lv_event_send(slider, LV_EVENT_VALUE_CHANGED, NULL);      /*To refresh the text*/
-    if(lv_obj_get_width(slider) > fit_w) lv_obj_set_width(slider, fit_w);
-
-}
-
 
 
