5b73a6d21e50d9d15fff4f3b3c0f2a83f2d78342
AlesCicero <a.cicero@studenti.unibg.it>
2022-10-10 15:15:56 +0200

    Simple button

--- lvgl/lv_examples/src/lv_demo_widgets/lv_demo_widgets.c
+++ lvgl/lv_examples/src/lv_demo_widgets/lv_demo_widgets.c
@@ -60,41 +60,56 @@ static lv_style_t style_box;
  *   GLOBAL FUNCTIONS
  **********************/
 
+static void event_handler(lv_obj_t* obj, lv_event_t event)
+{
+    if (event == LV_EVENT_CLICKED) {
+        lv_label_set_text(obj, "CLICCATO");
+    }
+}
+
+
 void lv_demo_widgets(void)
 {
-    tv = lv_tabview_create(lv_scr_act(), NULL);
-#if LV_USE_THEME_MATERIAL
-    if(LV_THEME_DEFAULT_INIT == lv_theme_material_init) {
-        lv_disp_size_t disp_size = lv_disp_get_size_category(NULL);
-        if(disp_size >= LV_DISP_SIZE_MEDIUM) {
-            lv_obj_set_style_local_pad_left(tv, LV_TABVIEW_PART_TAB_BG, LV_STATE_DEFAULT, LV_HOR_RES / 2);
-            lv_obj_t * sw = lv_switch_create(lv_scr_act(), NULL);
-            lv_obj_set_event_cb(sw, color_chg_event_cb);
-            lv_obj_set_pos(sw, LV_DPX(10), LV_DPX(10));
-            lv_obj_set_style_local_value_str(sw, LV_SWITCH_PART_BG, LV_STATE_DEFAULT, "Dark");
-            lv_obj_set_style_local_value_align(sw, LV_SWITCH_PART_BG, LV_STATE_DEFAULT, LV_ALIGN_OUT_RIGHT_MID);
-            lv_obj_set_style_local_value_ofs_x(sw, LV_SWITCH_PART_BG, LV_STATE_DEFAULT, LV_DPI/35);
-        }
-    }
-#endif
+    lv_obj_t* label;
+    lv_obj_t* btn1 = lv_btn_create(lv_scr_act(), NULL);
+    lv_obj_set_event_cb(btn1, event_handler);
+    lv_obj_align(btn1, NULL, LV_ALIGN_CENTER, 0, -40);
+    label = lv_label_create(btn1, NULL);
+    lv_label_set_text(label, "Button");
 
-    t1 = lv_tabview_add_tab(tv, "ALESSIO");
+
+    //controls_create(t1);
+
+//    tv = lv_tabview_create(lv_scr_act(), NULL);
+//#if LV_USE_THEME_MATERIAL
+//    if(LV_THEME_DEFAULT_INIT == lv_theme_material_init) {
+//        lv_disp_size_t disp_size = lv_disp_get_size_category(NULL);
+//        if(disp_size >= LV_DISP_SIZE_MEDIUM) {
+//            lv_obj_set_style_local_pad_left(tv, LV_TABVIEW_PART_TAB_BG, LV_STATE_DEFAULT, LV_HOR_RES / 2);
+//            lv_obj_t * sw = lv_switch_create(lv_scr_act(), NULL);
+//            lv_obj_set_event_cb(sw, color_chg_event_cb);
+//            lv_obj_set_pos(sw, LV_DPX(10), LV_DPX(10));
+//            lv_obj_set_style_local_value_str(sw, LV_SWITCH_PART_BG, LV_STATE_DEFAULT, "Dark");
+//            lv_obj_set_style_local_value_align(sw, LV_SWITCH_PART_BG, LV_STATE_DEFAULT, LV_ALIGN_OUT_RIGHT_MID);
+//            lv_obj_set_style_local_value_ofs_x(sw, LV_SWITCH_PART_BG, LV_STATE_DEFAULT, LV_DPI/35);
+//        }
+//    }
+//#endif
+
+    //t1 = lv_tabview_add_tab(tv, "ALESSIO");
     //t2 = lv_tabview_add_tab(tv, "Visuals");
     //t3 = lv_tabview_add_tab(tv, "Selectors");
 
 
-    lv_style_init(&style_box);
-    lv_style_set_value_align(&style_box, LV_STATE_DEFAULT, LV_ALIGN_OUT_TOP_LEFT);
-    lv_style_set_value_ofs_y(&style_box, LV_STATE_DEFAULT, - LV_DPX(10));
-    lv_style_set_margin_top(&style_box, LV_STATE_DEFAULT, LV_DPX(30));
+    //lv_style_init(&style_box);
+    //lv_style_set_value_align(&style_box, LV_STATE_DEFAULT, LV_ALIGN_OUT_TOP_LEFT);
+    //lv_style_set_value_ofs_y(&style_box, LV_STATE_DEFAULT, - LV_DPX(10));
+    //lv_style_set_margin_top(&style_box, LV_STATE_DEFAULT, LV_DPX(30));
 
-    controls_create(t1);
+    
     //visuals_create(t2);
     //selectors_create(t3);
 
-#if LV_DEMO_WIDGETS_SLIDESHOW
-    //lv_task_create(tab_changer_task_cb, 8000, LV_TASK_PRIO_LOW, NULL);
-#endif
 
 }
 
@@ -133,7 +148,9 @@ static void controls_create(lv_obj_t * parent)
     lv_obj_set_width(btn, lv_obj_get_width_grid(h, disp_size <= LV_DISP_SIZE_SMALL ? 1 : 2, 1));
     lv_obj_t * label = lv_label_create(btn, NULL);
     lv_label_set_text(label ,"Button");
-    lv_obj_set_event_cb(btn, kb_event_cb);
+    
+    //lv_obj_set_event_cb(btn, kb_event_cb);
+    
     //lv_checkbox_create(h, NULL);
 
     lv_coord_t fit_w = lv_obj_get_width_fit(h);
@@ -158,124 +175,4 @@ static void controls_create(lv_obj_t * parent)
 }
 
 
-static void slider_event_cb(lv_obj_t * slider, lv_event_t e)
-{
-    if(e == LV_EVENT_VALUE_CHANGED) {
-        if(lv_slider_get_type(slider) == LV_SLIDER_TYPE_NORMAL) {
-            static char buf[16];
-            lv_snprintf(buf, sizeof(buf), "%d", lv_slider_get_value(slider));
-            lv_obj_set_style_local_value_str(slider, LV_SLIDER_PART_KNOB, LV_STATE_DEFAULT, buf);
-        } else {
-            static char buf[32];
-            lv_snprintf(buf, sizeof(buf), "%d-%d", lv_slider_get_left_value(slider), lv_slider_get_value(slider));
-            lv_obj_set_style_local_value_str(slider, LV_SLIDER_PART_INDIC, LV_STATE_DEFAULT, buf);
-        }
 
-    }
-
-}
-
-static void ta_event_cb(lv_obj_t * ta, lv_event_t e)
-{
-    if(e == LV_EVENT_FOCUSED) {
-        if(kb == NULL) {
-            lv_obj_set_height(tv, LV_VER_RES / 2);
-            kb = lv_keyboard_create(lv_scr_act(), NULL);
-            lv_obj_set_event_cb(kb, kb_event_cb);
-        }
-        lv_textarea_set_cursor_hidden(ta, false);
-        lv_page_focus(t1, lv_textarea_get_label(ta), LV_ANIM_ON);
-        lv_keyboard_set_textarea(kb, ta);
-    } else if(e == LV_EVENT_DEFOCUSED) {
-        lv_textarea_set_cursor_hidden(ta, true);
-    }
-}
-
-static void kb_event_cb(lv_obj_t * _kb, lv_event_t e)
-{
-    lv_keyboard_def_event_cb(kb, e);
-
-    if(e == LV_EVENT_CANCEL) {
-        if(kb) {
-            lv_obj_set_height(tv, LV_VER_RES);
-            lv_obj_del(kb);
-            kb = NULL;
-        }
-    }
-}
-
-static void table_event_cb(lv_obj_t * table, lv_event_t e)
-{
-    if(e == LV_EVENT_CLICKED) {
-        uint16_t row;
-        uint16_t col;
-        lv_res_t r = lv_table_get_pressed_cell(table, &row, &col);
-        if(r == LV_RES_OK && col && row) {  /*Skip the first row and col*/
-            if(lv_table_get_cell_type(table, row, col) == 1) {
-                lv_table_set_cell_type(table, row, col, 4);
-            } else {
-                lv_table_set_cell_type(table, row, col, 1);
-            }
-        }
-    }
-}
-
-#if LV_USE_THEME_MATERIAL
-static void color_chg_event_cb(lv_obj_t * sw, lv_event_t e)
-{
-    if(LV_THEME_DEFAULT_INIT != lv_theme_material_init) return;
-    if(e == LV_EVENT_VALUE_CHANGED) {
-        uint32_t flag = LV_THEME_MATERIAL_FLAG_LIGHT;
-        if(lv_switch_get_state(sw)) flag = LV_THEME_MATERIAL_FLAG_DARK;
-
-        LV_THEME_DEFAULT_INIT(lv_theme_get_color_primary(), lv_theme_get_color_primary(),
-                flag,
-                lv_theme_get_font_small(), lv_theme_get_font_normal(), lv_theme_get_font_subtitle(), lv_theme_get_font_title());
-    }
-}
-#endif
-
-#if LV_DEMO_WIDGETS_SLIDESHOW
-
-static void tab_content_anim_create(lv_obj_t * parent)
-{
-    lv_anim_t a;
-    lv_obj_t * scrl = lv_page_get_scrl(parent);
-    lv_coord_t y_start = lv_obj_get_style_pad_top(parent, LV_PAGE_PART_BG);
-    lv_coord_t anim_h = lv_obj_get_height(scrl) - lv_obj_get_height_fit(parent);
-    uint32_t anim_time = lv_anim_speed_to_time(LV_DPI, 0, anim_h);
-
-    lv_anim_init(&a);
-    lv_anim_set_var(&a, scrl);
-    lv_anim_set_exec_cb(&a, (lv_anim_exec_xcb_t)lv_obj_set_y);
-    lv_anim_set_values(&a, y_start, y_start - anim_h);
-    lv_anim_set_time(&a, anim_time);
-    lv_anim_set_playback_time(&a, anim_time);
-    lv_anim_set_playback_delay(&a, 200);
-    lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
-    lv_anim_set_repeat_delay(&a, 200);
-    lv_anim_start(&a);
-}
-static void tab_changer_task_cb(lv_task_t * task)
-{
-    uint16_t act = lv_tabview_get_tab_act(tv);
-    act++;
-    if(act >= 3) act = 0;
-
-    lv_tabview_set_tab_act(tv, act, LV_ANIM_ON);
-
-    switch(act) {
-    case 0:
-        tab_content_anim_create(t1);
-        break;
-    case 1:
-        tab_content_anim_create(t2);
-        break;
-    case 2:
-        tab_content_anim_create(t3);
-        break;
-    }
-}
-#endif
-
-#endif
