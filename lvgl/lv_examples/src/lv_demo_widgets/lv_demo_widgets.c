4707b8a46d786bb3d1b9a96d98087d9a9ff5ff2a
AlesCicero <a.cicero@studenti.unibg.it>
2022-10-10 15:24:11 +0200

    Modify

--- lvgl/lv_examples/src/lv_demo_widgets/lv_demo_widgets.c
+++ lvgl/lv_examples/src/lv_demo_widgets/lv_demo_widgets.c
@@ -62,8 +62,8 @@ static lv_style_t style_box;
 
 static void event_handler(lv_obj_t* obj, lv_event_t event)
 {
-    if (event == LV_EVENT_CLICKED) {
-        lv_label_set_text(obj, "CLICCATO");
+    if (event == LV_EVENT_VALUE_CHANGED) {
+        
     }
 }
 
@@ -71,12 +71,16 @@ static void event_handler(lv_obj_t* obj, lv_event_t event)
 void lv_demo_widgets(void)
 {
     lv_obj_t* label;
-    lv_obj_t* btn1 = lv_btn_create(lv_scr_act(), NULL);
-    lv_obj_set_event_cb(btn1, event_handler);
-    lv_obj_align(btn1, NULL, LV_ALIGN_CENTER, 0, -40);
-    label = lv_label_create(btn1, NULL);
-    lv_label_set_text(label, "Button");
+    
+    lv_obj_t* btn2 = lv_btn_create(lv_scr_act(), NULL);
+    lv_obj_set_event_cb(btn2, event_handler);
+    lv_obj_align(btn2, NULL, LV_ALIGN_CENTER, 0, 100);
+    lv_btn_set_checkable(btn2, true);
+    lv_btn_toggle(btn2);
+    lv_btn_set_fit2(btn2, LV_FIT_NONE, LV_FIT_TIGHT);
 
+    label = lv_label_create(btn2, NULL);
+    lv_label_set_text(label, "Toggled");
 
     //controls_create(t1);
 
